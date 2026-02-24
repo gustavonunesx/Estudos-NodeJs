57d16c153989a8e82bec99ce6e15c1b49f15f099
= <=>
2026-02-24 16:02:29 -0300

    logica de rodadas e blocos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log("ui!");
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+}
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+}
+
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+
+async function getRandomBlock() {
+    let random = Math.random()
+    let result 
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+
+        case random < 0.66:
+            result = "CURVA";
+            break
+    
+        default:
+            result = "CONFRONTO";
+    }
+
+    return result;
+    
+}
+async function playRaceEngine(character1, character2) {
+
+    for (let round = 1; round <= 5; round++){
+        console.log(`🏁 Rodada ${round}`);
+
+        //sortear bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco ${block}`)
+    }
+
+
+}
+
+
+//funcao auto invoke
+(async function main(){
+    console.log(             //template string ⬇️
+        `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`)
+
+        await playRaceEngine(player1,player2);
+})();

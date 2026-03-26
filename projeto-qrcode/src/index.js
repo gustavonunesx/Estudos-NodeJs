372a15daa480d4cda63b4e471265d231bc5a156a
= <=>
2026-03-25 22:09:30 -0300

    main prompt

--- projeto-qrcode/src/index.js
+++ projeto-qrcode/src/index.js
@@ -1,8 +1,13 @@
 import prompt from 'prompt'
-
+import mainPrompt from './prompts/prompt-main.js'
 
 async function main() {
-    
+    prompt.get(mainPrompt, async(err, choice) => {
+        if(choice.select == 1) console.log("escolheu QRCODE");
+        if(choice.select == 2) console.log("escolheu PASSWORD");
+    });     
+
+    prompt.start();
 }
 
 
